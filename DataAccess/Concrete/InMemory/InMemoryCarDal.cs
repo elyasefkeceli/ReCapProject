ce7b7f17be137ae5cbda137ee268eb7b79382a38
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-04-08 21:21:58 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car { Id = 1, BrandId = 1, ColorId = 1, ModelYear = "2019", DailyPrice = 210000, Description = "Audi A1" },
+            new Car { Id = 2, BrandId = 2, ColorId = 1, ModelYear = "2020", DailyPrice = 1200000, Description = "Tesla S" },
+            new Car { Id = 3, BrandId = 3, ColorId = 2, ModelYear = "2018", DailyPrice = 300000, Description = "Hundai Civic" },
+            new Car { Id = 4, BrandId = 4, ColorId = 2, ModelYear = "2017", DailyPrice = 500000, Description = "Ford Mustang" },
+            new Car { Id = 5, BrandId = 5, ColorId = 3, ModelYear = "2016", DailyPrice = 120000, Description = "Ford Focus" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c=>c.Id==c.Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
