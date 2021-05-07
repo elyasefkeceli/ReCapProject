8c53839a3069d5c8d2d3c29c752220db469e8c7d
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-05-07 18:38:51 +0300

    07.05.2021

---
+++ Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CustomerValidator :AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.CompanyName).NotEmpty();
+            RuleFor(c => c.CompanyName).MinimumLength(2);
+           // RuleFor(c => c.CompanyName).Must(StartWithA);
+        }
+
+        //private bool StartWithA(string arg)
+        //{
+        //    return arg.StartsWith('A');
+        //}
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator :AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(5).WithMessage("password can not be emty");
+            RuleFor(u => u.FirstName).NotEmpty().MinimumLength(3);
+            RuleFor(u => u.LastName).NotEmpty().MinimumLength(2);
+            
+        }
+    }
+}
